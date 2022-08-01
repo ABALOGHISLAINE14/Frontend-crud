40c4a14749aa1bdd9cb19136e1270f15ed720749
ABALOGHISLAINE14 <sylvieabalo48gmail.com>
2022-08-01 15:38:10 +0200

    Premier commit crud

---
+++ movies.js
@@ -0,0 +1,28 @@
+export const movies = [
+    {
+      name: "Adebayor",
+      wins: 2014,
+      Loses: 3,
+      Scores:12
+    },{
+        name: "Djene",
+        wins: 2014,
+        Loses: 3,
+        Scores:12
+      },{
+        name: "Agassa",
+        wins: 2014,
+        Loses: 3,
+        Scores:12
+      },{
+        name: "Gagpe",
+        wins: 2014,
+        Loses: 3,
+        Scores:12
+      },{
+        name: "Interstellar",
+        wins: 2014,
+        Loses: 3,
+        Scores:12
+      }
+  ];
